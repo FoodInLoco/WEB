4ae1935d7cd97eebc3a237220907b18abb3b6df7
Gabriel Evaristo <gabrielevaristovcp@gmail.com>
2022-09-09 10:06:50 -0300

    automatic deploy

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import { Navbar } from "../components/NavBar";
 import { Home } from "../pages/home";
 export const AppRoutes = () => {
   return (
