dedbb7feeb4c20f904137727722f77e3b2291d4e
Pedro Lopes de Oliveira <labsit.poliveira@creditas.com>
2022-11-28 18:29:38 -0300

    Restaurant page

--- src/components/NavMenuRestaurant/index.tsx
+++ src/components/NavMenuRestaurant/index.tsx
@@ -6,8 +6,11 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { Avatar, Skeleton } from '@mui/material';
-
+import { Avatar, Card, CardContent, Skeleton, Table, TableContainer } from '@mui/material';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 
 
@@ -57,61 +60,92 @@ export function TabRestaurant({ menus }: { menus: Menu[] | undefined }) {
     setValue(index);
   };
 
+  const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  });
+
   return (
-    <Box sx={{ bgcolor: 'background.paper', width: '100%' }}>
-      {menus ? <>
-        <AppBar position="static">
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            indicatorColor="secondary"
-            textColor="inherit"
-            variant="fullWidth"
-            aria-label="full width tabs example"
-          >{
+    <Card sx={{ bgcolor: 'background.paper', width: '100%', minHeight: '93vh', maxHeight: '93vh' }}>
+      <CardContent sx={{ minHeight: '62vh' }}>
+        {menus ? <>
+          <AppBar position="static">
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="secondary"
+              textColor="inherit"
+              variant="fullWidth"
+              aria-label="full width tabs example"
+            >{
+                menus.map((menu, index) => (
+                  <Tab iconPosition="start" icon={menu.photo && <Avatar src={menu.photo} sx={{ width: 30, height: 30 }}/>} label={menu.name} {...a11yProps(index)}/>
+                ))
+              }
+            </Tabs>
+          </AppBar>
+          <SwipeableViews
+            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            {
               menus.map((menu, index) => (
-                <Tab label={menu.name} {...a11yProps(index)} />
+                <TabPanel value={value} index={index} dir={theme.direction}>
+                  <Typography variant="h5" gutterBottom component="div">
+                    {menu.description}
+                  </Typography>
+                  <Typography gutterBottom component="div">
+                    {menu.happyHour && "Horário do happy hour: " + menu.startAt + " - " + menu.endAt}
+                  </Typography>
+                  <br />
+                  <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+                        <TableCell component="th" scope="row">
+                        </TableCell>
+                        <TableCell>Nome</TableCell>
+                        <TableCell>Descrição</TableCell>
+                        <TableCell align="center">Porção</TableCell>
+                        <TableCell align="center">Preço</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {menu.items.map((item, index) => (
+                        <TableRow key={index}>
+                          <TableCell component="th" scope="row">
+                            {item.photo}
+                          </TableCell>
+                          <TableCell>{item.name}</TableCell>
+                          <TableCell>{item.description}</TableCell>
+                          <TableCell align="center">{item.quantity}</TableCell>
+                          <TableCell align="center">
+                            {formatter.format(item.value)}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TabPanel>
               ))
             }
+          </SwipeableViews>
+        </> : <div
+          style={{
+            marginRight: 20,
+            marginLeft: 20
+          }}>
+          <Skeleton animation="wave"
+            height={50}
+            width="100%"
 
-          </Tabs>
-        </AppBar>
-        <SwipeableViews
-          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-          index={value}
-          onChangeIndex={handleChangeIndex}
-        >
-
-          {
-            menus.map((menu, index) => (
-              <TabPanel value={value} index={index} dir={theme.direction}>
-                {menu.photo && <Avatar src={menu.photo} sx={{ width: 60, height: 60 }} />}
-                {menu.items.map((menu, index) => (
-                  <>
-                    <span>Prato: </span>
-                    <span>{menu.name}</span>
-                    <br />
-                  </>
-                ))}
-              </TabPanel>
-            ))
-          }
-        </SwipeableViews>
-      </> : <div
-        style={{
-          marginRight: 20,
-          marginLeft: 20
-        }}>
-        <Skeleton animation="wave"
-          height={50}
-          width="100%"
-
-        />
-        <Skeleton animation="wave"
-          height={500}
-          width="100%"
-        />
-      </div>}
-    </Box>
+          />
+          <Skeleton animation="wave"
+            height={500}
+            width="100%"
+          />
+        </div>}
+      </CardContent>
+    </Card>
   );
 }
--- src/components/RestaurantInfo/index.tsx
+++ src/components/RestaurantInfo/index.tsx
@@ -1,7 +1,13 @@
-import { Avatar, Card, CardActionArea, CardContent, CardHeader, Container, IconButton, Skeleton, Typography } from "@mui/material";
+import { Avatar, Box, Card, CardActionArea, CardContent, CardHeader, Container, IconButton, Skeleton, Tab, Tabs, Typography } from "@mui/material";
 import { red } from "@mui/material/colors";
+import { useTheme } from '@mui/material/styles';
+import ContactsIcon from '@mui/icons-material/Contacts';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import PlaceIcon from '@mui/icons-material/Place';
 import { handleAddress } from "../../utils/formatAddress";
 import { handlePhone } from "../../utils/formatPhone";
+import React from "react";
+import { Email, Phone } from "@mui/icons-material";
 interface IRestaurantInfo {
   isLoading: boolean;
   restaurant: IRestaurantProps | undefined | null
@@ -24,6 +30,53 @@ export function RestaurantInfo({ isLoading, restaurant }: IRestaurantInfo) {
       </Avatar>
     }
   }
+  
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
+  interface TabPanelProps {
+    children?: React.ReactNode;
+    dir?: string;
+    index: number;
+    value: number;
+  }
+  
+  function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`full-width-tabpanel-${index}`}
+        aria-labelledby={`full-width-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  function a11yProps(index: number) {
+    return {
+      id: `full-width-tab-${index}`,
+      'aria-controls': `full-width-tabpanel-${index}`,
+    };
+  }
+
+  const theme = useTheme();
+  
   return <Container sx={{ borderRadius: 5, minHeight: '60vh' }}>
     <Card variant="outlined">
       <CardHeader
@@ -48,37 +101,22 @@ export function RestaurantInfo({ isLoading, restaurant }: IRestaurantInfo) {
         }
       />
       <CardContent sx={{ minHeight: '62vh' }}>
-        {isLoading ?
-          <>
-            <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
-            <Skeleton animation="wave" height={10} width="80%" />
-          </> :
-          <Typography variant="h6">
-            Contato:
-            <br />
-            ({restaurant?.ddd}) {phoneNumber} - {restaurant?.email}
-          </Typography>}
-        {isLoading ?
-          <>
-            <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
-            <Skeleton animation="wave" height={10} width="80%" />
-          </> :
-          <Typography variant="h6">
-            Endereço:
-            <br />
-            {address}
-          </Typography>}
-
-        {/* {isLoading ?
-          <>
-            <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
-            <Skeleton animation="wave" height={10} width="80%" />
-          </> :
-          <Typography variant="h6">
-            Horário de funcionamento:
-            <br />
-            ({restaurant?.ddd}) {restaurant?.phoneNumber} - {restaurant?.email}
-          </Typography>} */}
+        <Tabs value={value} onChange={handleChange} variant="fullWidth" aria-label="icon tabs example">
+          <Tab icon={<ContactsIcon />} label="Contatos" {...a11yProps(0)} />
+          <Tab icon={<PlaceIcon />} label="Endereço" {...a11yProps(1)} />
+          <Tab icon={<AccessTimeIcon />} label="Horário" {...a11yProps(2)} />
+        </Tabs>
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          <Phone /> ({restaurant?.ddd}) {phoneNumber}
+          <br/>
+          <Email /> {restaurant?.email}
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          {address}
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+          
+        </TabPanel>
       </CardContent>
     </Card>
   </Container>
